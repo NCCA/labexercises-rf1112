ba79c622909c2d67248915232b0507f360f8f724
rf1112 <rina.fumoto@icloud.com>
2022-05-31 11:58:01 +0100

    Added videos

--- build-ParticleGUI-Desktop_Qt_GCC_64bit-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/opt/Qt5.15.2/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/opt/QT/5.15.2/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/opt/rh/devtoolset-9/root/usr/bin/gcc" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/opt/rh/devtoolset-9/root/usr/bin/g++" CACHE "STRING" "" FORCE)
